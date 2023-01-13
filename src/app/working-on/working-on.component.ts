c99968095efd67f09b4deef10f750d5e3873ea79
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-01-13 10:58:55 +0100

    feat(working-on): remove ViewEncapsulation.None

--- src/app/working-on/working-on.component.ts
+++ src/app/working-on/working-on.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-working-on',
   templateUrl: './working-on.component.html',
   styleUrls: ['./working-on.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class WorkingOnComponent {
   workingOnForm = new FormGroup({
