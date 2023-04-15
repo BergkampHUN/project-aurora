447b6e6dff80dc910d52753fb0d74c46abb4c421
BergkampHUN <akostm@gmail.com>
2023-04-15 20:50:20 +0200

    feat(WorkingOn): add start stop logic and parse running timer

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,17 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { MiteService } from '../services/mite.service';
+import { Subscription } from 'rxjs';
+import { TrackingTimeEntryWithId } from '../interfaces/mite';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss'],
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnInit, OnDestroy {
+  @Input()
+  public type!: 'mite' | 'timetac';
+
   public hour: any = '0' + 0;
   public minute: any = '0' + 0;
-  public second: any = '0' + 0;
 
-  startTimer: any;
-  running = false;
+  public startTimer: any;
+  public running = false;
 
-  ngOnInit(): void {}
+  public currentTracker$!: Subscription;
+
+  constructor(private miteService: MiteService) {}
+
+  ngOnInit(): void {
+    if (this.type === 'mite') {
+      this.currentTracker$ = this.miteService.currentTracker$.subscribe((tracker) => {
+        if (tracker && tracker.tracking_time_entry) {
+          console.log(tracker);
+          this.formatMiteResponse(tracker.tracking_time_entry);
+        } else {
+          this.clearTimer();
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.currentTracker$.unsubscribe();
+  }
+
+  private formatMiteResponse(entry: TrackingTimeEntryWithId): void {
+    const minutes = entry.minutes;
+    const hours = Math.floor(minutes / 60).toString();
+    const remainingMinutes = (minutes % 60).toString();
+    this.hour = hours.padStart(2, '0');
+    this.minute = remainingMinutes.padStart(2, '0');
+  }
+
+  private clearTimer(): void {
+    this.hour = '0'.padStart(2, '0');
+    this.minute = '0'.padStart(2, '0');
+  }
 }
